ffdb95c35eb6f383dbb1ee135eab541d48d02e97
Tim Lupo <socialmedia@timlupo.com>
2016-01-11 10:23:21 -0500

    Lesson 1
    Lesson 1 introduces the basics concepts of Unity3D, the game we will be
    making (pong), and starts by importing and programming paddles to move
    up and down in a fixed space.

---
+++ Pong/Assets/Paddle1.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+
+var speed = 5;
+
+function Start () {
+
+}
+
+function Update () {
+
+	if (Input.GetKey(KeyCode.W)) {
+		transform.Translate(Vector2.up*Time.deltaTime*speed);
+	} if (Input.GetKey(KeyCode.S)) {
+		transform.Translate(Vector2.down*Time.deltaTime*speed);
+	} if (Input.GetKey(KeyCode.A)) {
+		transform.Translate(Vector2.left*Time.deltaTime*speed);
+	} if (Input.GetKey(KeyCode.D)) {
+		transform.Translate(Vector2.right*Time.deltaTime*speed);
+	}
+
+	if (transform.position.y > 3.25) {
+		transform.position.y = 3.25;
+	} else if (transform.position.y < -3.25) {
+		transform.position.y = -3.25;
+	}
+}
---
+++ Pong/Assets/Paddle2.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+var speed = 5;
+
+function Start () {
+
+}
+
+function Update () {
+
+	if (Input.GetKey(KeyCode.UpArrow)) {
+		transform.Translate(Vector2.up*Time.deltaTime*speed);
+	} if (Input.GetKey(KeyCode.DownArrow)) {
+		transform.Translate(Vector2.down*Time.deltaTime*speed);
+	}
+
+	if (transform.position.y > 3.25) {
+		transform.position.y = 3.25;
+	} else if (transform.position.y < -3.25) {
+		transform.position.y = -3.25;
+	}
+}
