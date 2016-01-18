0f05f84c39350702b8561e9ef1a5e1e0babdf9f4
Tim Lupo <socialmedia@timlupo.com>
2016-01-18 16:24:23 -0500

    Lesson 2
    Lesson 2 adds the ball, properties of the ball, scoring, and other game
    keeping features.

---
+++ Pong/Assets/ball.js
@@ -0,0 +1,46 @@
+﻿#pragma strict
+
+var score1: int;
+var score2: int;
+var player1: GameObject;
+var player2: GameObject;
+
+function Start () {
+	Launch(150, 150);
+}
+
+function Update () {
+	if (transform.position.x > 10) {
+		Launch(150,150);
+		score1 += 1;
+		Debug.Log("Player 1 Scored!\nPlayer 1 = " + score1 + " | Player 2 = " + score2);
+	} else if (transform.position.x < -10) {
+		Launch(150,150);
+		score2 += 1;
+		Debug.Log("Player 2 Scored!\nPlayer 1 = " + score1 + " | Player 2 = " + score2);
+	}
+}
+
+function Launch (x: int, y: int) {
+	var random: int = Random.Range(0,4);
+	switch (random) {
+		case 0:
+			break;
+		case 1:
+			y = y*-1;
+			break;
+		case 2:
+			x = x*-1;
+			break;
+		case 3:
+			x = x*-1;
+			y = y*-1;
+			break;
+	}
+	GetComponent.<Rigidbody2D>().velocity = Vector2.zero;
+	transform.position = new Vector2(0,0);
+	player1.transform.position.y = 0;
+	player2.transform.position.y = 0;
+	yield WaitForSeconds(2);
+	GetComponent.<Rigidbody2D>().AddForce(new Vector2(x,y));
+}
